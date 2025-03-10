54964020616fa636dc57fe90247063ceab748e2e
alvillage <azuknisianta@gmail.com>
2025-03-11 05:41:33 +0700

    ## - test with result

--- src/main.rs
+++ src/main.rs
@@ -38,9 +38,23 @@ fn starting_app_test() {
 }
 
 #[test]
-#[ignore]
 // Indicates that this test will be ignored by default when running `cargo test`.
 // To run ignored tests, use `cargo test -- --ignored`.
+#[ignore]
 fn ignored_test() {
     println!("This test is ignored.");
+}
+
+#[test]
+// Defines a test function named `addition_with_result_test`, which returns a `Result` type.
+// If the test passes, it returns `Ok(())`, otherwise, it returns an `Err` with an error message.
+fn addition_with_result_test()->Result<(),String> {
+    let result = addition(10, 20);
+    if result == 30 {
+        // If the condition is met, the test passes by returning `Ok(())`.
+        Ok(())
+    }else {
+        // If the condition is not met, the test fails by returning an error message as `Err`.
+        Err("Addition result should be 30".to_string())
+    }
 }
