71b83ff94e2e33b6f487368d3c735b2cad85da17
alvillage <azuknisianta@gmail.com>
2025-03-11 05:03:30 +0700

    ## - first assertion test

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,13 @@ fn main() {
 fn simple_test() {
     println!("Hello, world!");
 }
+
+fn addition(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn addition_test() {
+    let result = addition(10, 20);
+    assert_eq!(result, 30,"10 + 20 its should be 30");
+}
