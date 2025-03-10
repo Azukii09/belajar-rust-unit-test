4d0471315d63f15dc48221fd8b78029f0cc5e6be
alvillage <azuknisianta@gmail.com>
2025-03-11 05:09:16 +0700

    ## - panic

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,19 @@ fn addition(a: i32, b: i32) -> i32 {
 #[test]
 fn addition_test() {
     let result = addition(10, 20);
-    assert_eq!(result, 30,"10 + 20 its should be 30");
+    assert_eq!(result, 30,"10 + 20 it's should be 30");
+}
+
+fn start_application(host:&str,port:u16){
+    if host == "localhost"{
+        panic!("You must not use localhost");
+    }else { 
+        println!("Starting application in host {}:{}...",host,port);
+    }
+}
+
+#[test]
+#[should_panic]
+fn starting_app_test() {
+    start_application("localhost",8000);
 }
